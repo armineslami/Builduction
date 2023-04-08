4ddbb7b479874236cde22f4d01ba68b965f7eefb
Armin Eslami <armineslami@Armins-MacBook-Air.local>
2023-04-08 22:07:00 +0330

    Add splash screen images and links for iOS devices

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -13,9 +13,6 @@ const Head = () => {
       <meta name="theme-color" content="#000000" />
       <link rel="icon" href="/images/favicon.ico" />
 
-      {/* PWA manifest */}
-      <link rel="manifest" href="/manifest.json" />
-
       {/* Apple devices home screen icons */}
       <link rel="apple-touch-icon" href="/images/icon-192x192.png" />
       <link
@@ -32,6 +29,68 @@ const Head = () => {
         rel="apple-touch-icon"
         sizes="167x167"
         href="/images/touch-icon-ipad-retina-167.png"
+      />
+
+      {/* PWA */}
+
+      <link rel="manifest" href="/manifest.json" />
+
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-touch-fullscreen" content="yes" />
+      <meta name="apple-mobile-web-app-title" content="Builduction" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+
+      {/* PWA Splash screen for iOS */}
+      <link
+        href="/images/splashscreens/iphone5_splash.png"
+        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="/images/splashscreens/iphone6_splash.png"
+        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="/images/splashscreens/iphoneplus_splash.png"
+        media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="/images/splashscreens/iphonex_splash.png"
+        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="/images/splashscreens/iphonexr_splash.png"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="/images/splashscreens/iphonexsmax_splash.png"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="/images/splashscreens/ipad_splash.png"
+        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="/images/splashscreens/ipadpro1_splash.png"
+        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="/images/splashscreens/ipadpro3_splash.png"
+        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="/images/splashscreens/ipadpro2_splash.png"
+        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
       />
     </NextHead>
   );
