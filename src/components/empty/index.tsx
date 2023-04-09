b872dc37616116902b3e41c3bd7270ff7270371c
Armin Eslami <armineslami@hotmail.com>
2023-04-09 14:27:26 +0330

    Add 'priority=ture' for empty component's image.

--- src/components/empty/index.tsx
+++ src/components/empty/index.tsx
@@ -1,11 +1,17 @@
-import { Text, VStack, Image } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
+import Image from "next/image";
 import { FunctionComponent } from "react";
 
 const Empty: FunctionComponent = () => {
   return (
     <div className="center">
       <VStack w="200px" h="200px">
-        <Image alt="empty" src="/images/directory.png" w="100px" h="100px" />
+        <Image
+          priority={true}
+          alt="empty"
+          src="/images/directory.png"
+          style={{ width: "100px", height: "100px" }}
+        />
         <Text>پروژه‌ای وجود ندارد</Text>
       </VStack>
     </div>
