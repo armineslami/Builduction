4a621a00dddcbf483665f85b2b1b41ddf094b8a1
Armin Eslami <armineslami@hotmail.com>
2023-04-11 09:59:59 +0330

    Exclude footer for 404 page.

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,16 +1,20 @@
 import { FunctionComponent } from "react";
 import Footer from "@/components/footer";
 import Head from "@/components/head";
+import { useRouter } from "next/router";
 
-const MainLayout: FunctionComponent<any> = ({children}) => {
-    return (
-        <>
-            <Head />
-            {/* <Header /> */}
-            <>{children}</>
-            <Footer />
-        </>
-    );
-}
+const MainLayout: FunctionComponent<any> = ({ children }) => {
+  const router = useRouter();
+  const isRouterOn404: boolean = String(router.pathname).startsWith("/404");
+
+  return (
+    <>
+      <Head />
+      {/* <Header /> */}
+      <>{children}</>
+      {!isRouterOn404 ? <Footer /> : undefined}
+    </>
+  );
+};
 
 export default MainLayout;
